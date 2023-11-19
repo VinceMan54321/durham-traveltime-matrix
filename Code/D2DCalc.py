234b60375f4e80b9053200e395c1f87c3c60fd6a
Darshan Vijaykumar <dv80@duke.edu>
2023-11-19 14:14:45 -0500

    Location Dataset and Working D2D calc

--- Code/D2DCalc.py
+++ Code/D2DCalc.py
@@ -1,39 +1,60 @@
 """
-Change output list from step 1 to a dictionary where the keys 
-are all locations and the values include all of the 
-other locations + travel times for a given location 
-- For example: {perkins: {wu : 2, sanford : 9, Trinity Dorm : 23 …} 
-- This is an example of the output for the specific key of perkins. It is 
-saying perkins to wu is 2 mins, perkins to sanford is 9 mins and perkins to 
-trinity is 23 mins 
 
 Task: Given a specific starting location, calculate the updated travel times with the following factors 
 - East vs west
 - Weekend or weekday (CSwift vs C1) 
 - Time of day 
 - Average time to wait for bus 
-- Think of more
+
 
 Output: a start location, and a list with each row containing a location, travel time, and grid number
 """
 
 #read csv 
 import pandas as pd
-#locations_df = pd.read_csv ('location_dataset')
+locations_df = pd.read_csv ('Location_Dataset.csv')
+campus = locations_df[locations_df["Name"] == "Hollows A"]['Campus']
 
 
+west_to_east = "WestEast"
+east_to_west = "EastWest"
 
-
-def door_to_door_updates(start, matrix): 
+def door_to_door_updates(start, matrix, weekday, time_of_day): 
     dict = matrix[start]
-    travel_time_df = pd.dataframe(list(dict.items()), Columns = ['Name', 'Time'])
+    travel_time_df = pd.DataFrame(list(dict.items()), columns = ['Name', 'Time'])
     merged_df = travel_time_df.merge(locations_df, how = "outer", on = "Name")
-    for ind in merged_df.index
-        #C1 wait time
-            #Check if weekday or weekend
-            #check if it is morning or night
-        #if dorm 
-            #add exit dorm time 
+    merged_df["Campus"] =  merged_df["Campus"].astype("string")
+    merged_df["Location Type"] =  merged_df["Location Type"].astype("string")
+    start_campus = locations_df[locations_df["Name"] == start]['Campus'].values[0]
+    start_type = locations_df[locations_df["Name"] == start]['Location Type'].values[0]
+    
+
+    for ind, row in merged_df.iterrows():
+        dest_campus = row["Campus"]
+        dest_type = row["Location Type"]
+        travel = start_campus + dest_campus
+        if travel == west_to_east or travel == east_to_west: 
+            if weekday: 
+                if time_of_day == "Night":
+                    merged_df.at[ind, "Time"] =  merged_df.at[ind, "Time"] + 5
+                else: 
+                    merged_df.iloc[ind]["Time"] =  merged_df.at[ind, "Time"] + 2
+            else: 
+                if time_of_day == "Night":
+                    merged_df.iloc[ind]["Time"] =  merged_df.at[ind, "Time"] + 11
+
+                else: 
+                    merged_df.iloc[ind]["Time"] =  merged_df.at[ind, "Time"] + 5.5
+        if start_type == "dorm" or dest_type == "dorm": 
+            merged_df.at[ind, "Time"] = merged_df.at[ind, "Time"] + 3
+            
+
+    ret_df = merged_df 
+    return ret_df
+        
+     
         
     
+
+
 
